c62e487c3b1e61ee92f89c8dd725694e6bd7bdc4
hajungIm <ihj_ewha@ewhain.net>
2024-03-29 22:47:20 +0900

    refactor: 목표 삭제 성능 개선
    Related to: #4

--- src/main/java/com/umc5th/muffler/domain/goal/repository/GoalRepository.java
+++ src/main/java/com/umc5th/muffler/domain/goal/repository/GoalRepository.java
@@ -1,12 +1,12 @@
 package com.umc5th.muffler.domain.goal.repository;
 
 import com.umc5th.muffler.entity.Goal;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
 import java.time.LocalDate;
 import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface GoalRepository extends JpaRepository<Goal, Long>, GoalRepositoryCustom {
     @Query("SELECT goal from Goal goal where :date BETWEEN goal.startDate and goal.endDate AND goal.member.id = :memberId")
@@ -20,4 +20,11 @@ public interface GoalRepository extends JpaRepository<Goal, Long>, GoalRepositor
 
     @Query("SELECT g FROM Goal g JOIN FETCH g.dailyPlans WHERE g.id = :goalId")
     Optional<Goal> findByIdAndFetchDailyPlans(Long goalId);
+
+    @Query("SELECT g FROM Goal g WHERE g.id = :goalId AND g.member.id = :memberId")
+    Optional<Goal> findByIdAndMemberId(Long goalId, String memberId);
+
+    @Modifying
+    @Query("DELETE FROM Goal g WHERE g.id = :goalId AND g.member.id = :memberId")
+    void deleteByIdAndMemberId(Long goalId, String memberId);
 }
